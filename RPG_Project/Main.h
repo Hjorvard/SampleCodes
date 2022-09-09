2cb21e63584e194caf45e6920a6e59b6af3de180
Hjorvard <111450930+Hjorvard@users.noreply.github.com>
2022-09-09 22:04:38 +0100

    Update Main.h

--- RPG_Project/Main.h
+++ RPG_Project/Main.h
@@ -7,12 +7,15 @@
 #include "Main.generated.h"
 
 
+
+
 UENUM(BlueprintType)
 //enumerator for the assorted variations of movement, such as walk, sprint, crouch
 enum class EMovementStatus : uint8
 {
 	EMS_Normal UMETA(DisplayName = "Normal"),
 	EMS_Sprinting UMETA(DisplayName = "Sprinting"),
+	EMS_Dead UMETA(DisplayName = "Dead"),
 
 	EMS_MAX UMETA(DisplayName = "DefaultMAX")
 
@@ -39,6 +42,10 @@ public:
 	// Sets default values for this character's properties
 	AMain();
 
+	//reference to the main player controller
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Controller")
+	class AMainPlayerController* MainPlayerController;
+
 	//the various states of movement the player can have
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Enums")
 	EMovementStatus MovementStatus;
@@ -56,6 +63,13 @@ public:
 	//release to disable sprinting
 	void ShiftKeyUp();
 
+	//bool for whether the has pressed escape or not
+	bool bESCDown;
+	//pressed to enable the pause menu
+	void ESCDown();
+	//release to disable the pause menu
+	void ESCUp();
+
 
 	//Stamina related info goes here
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Enums")
@@ -68,6 +82,9 @@ public:
 	//the minimum amount of stamina required to start sprinting
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
 	float MinSprintStamina;
+
+	//an override of the built in jump function
+	virtual void Jump() override;
 
 
 	/** Camera boom to automatically position the camera behind the player */
@@ -121,6 +138,7 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
 	int32 Coins;
 	//function for adding a coin after collecting
+	UFUNCTION(BlueprintCallable, Category = "Items")
 	void IncrementCoins(int32 Amount);
 
 
@@ -157,6 +175,34 @@ public:
 	//take in the vector of the enemy
 	FRotator GetLookAtRotationYaw(FVector Target);
 
+	//a bool to see if the player has a combat target
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat")
+	bool bHasCombatTarget;
+
+	//a setter for the combat target
+	FORCEINLINE void SetHasCombatTarget(bool HasTarget)
+	{
+		bHasCombatTarget = HasTarget;
+	}
+
+	//function to update the combat target for example if one enemy dies
+	void UpdateCombatTarget();
+
+	//subclass of enemies to use in a filter for combat targets
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+	TSubclassOf<AEnemy> EnemyFilter;
+
+	//property for getting the location of the current combat target
+	//use this in order to correctly place and scale the enemy health bar widget
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Combat")
+	FVector CombatTargetLocation;
+
+	//function to call at the end of the player death animation
+	UFUNCTION(BlueprintCallable)
+	void DeathEnd();
+
+
+
 
 	//
 	//
@@ -172,6 +218,9 @@ public:
 	void PickupUp();
 	//create a bool for if the button is held down
 	bool bPickupDown;
+	//function for healing health after picking up a potion
+	UFUNCTION(BlueprintCallable, Category = "Items")
+	void IncrementHealth(float Amount);
 
 
 protected:
@@ -190,6 +239,11 @@ public:
 
 	//called for left and right movement based on input
 	void MoveRight(float Value);
+
+	//bools for if the player is moving forward or right
+	//though since it handles movement on a negative scale this is essentially for any movement
+	bool bMovingForward;
+	bool bMovingRight;
 
 	//called from input to turn on the horizontal / x axis at set rate
 	//@param Rate is a normalised rate at 100% of desired turn rate
